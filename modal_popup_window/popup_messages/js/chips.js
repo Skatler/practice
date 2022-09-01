c119f5137ec586841e742d1d3e89ee4e00edca49
Skatler <yaroslavzozuliya95@gmail.com>
2022-09-01 14:46:14 +0300

    Finish

---
+++ modal_popup_window/popup_messages/js/chips.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+  document.querySelector("#chips-show").onclick = () => {
+    let text = document.querySelector("#chips-text").value;
+    // document.querySelector("#chips-text").value = "";
+    let chips = document.createElement("div");
+    chips.classList.add("chips");
+    chips.innerText = text;
+
+    // document.body.appendChild(chips);
+    if (!document.querySelector("#chips-field")) {
+      let chipsField = document.createElement("div");
+      chipsField.id ="chips-field";
+      document.body.appendChild(chipsField);
+    }
+    document.querySelector("#chips-field").appendChild(chips);
+    setTimeout(() => {
+        chips.remove();
+        removeChipsField();
+      }, 3000);
+    };
+    function removeChipsField() {
+      let chips = document.querySelectorAll("#chips-field .chips");
+      if (chips.length == 0) {
+        document.querySelector("#chips-field").remove();
+      }
+    }
+  };
